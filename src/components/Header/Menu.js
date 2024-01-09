e12635a5686c096ecc782342095de89d59ec3126
Daniel Pôrto Núñez <porto@Daniels-MacBook-Pro.local>
2024-01-09 14:22:39 -0300

    feat: add courses on header

--- src/components/Header/Menu.js
+++ src/components/Header/Menu.js
@@ -6,7 +6,7 @@ import { AnimatePresence, motion } from 'framer-motion'
 import { menu, menuItem } from './anim'
 import { anim } from '@/styles/helpers'
 
-export const Menu = ({ show }) => {
+export const Menu = ({ show, data }) => {
   return (
     <AnimatePresence>
       {show ? (
@@ -17,37 +17,38 @@ export const Menu = ({ show }) => {
           <div className="py-7">
             <h2 className="text-md text-black">Escolha seu curso</h2>
           </div>
-
-          <div className="flex justify-between gap-8">
-            {courseList.map((course, i) => (
-              <motion.div
-                {...anim(menuItem, i)}
-                key={course.name}
-                className="py-4 flex-1"
-              >
-                <CourseCard {...course} />
-              </motion.div>
-            ))}
-          </div>
+          {data && (
+            <div className="flex justify-between gap-8">
+              {data.map((course, i) => (
+                <motion.div
+                  {...anim(menuItem, i)}
+                  key={course.name}
+                  className="py-4 flex-1"
+                >
+                  <CourseCard {...course} />
+                </motion.div>
+              ))}
+            </div>
+          )}
         </motion.div>
       ) : null}
     </AnimatePresence>
   )
 }
 
-const CourseCard = ({ image, name, href }) => {
+const CourseCard = ({ cover, name, slug }) => {
   const [hover, setHover] = useState(false)
 
   return (
-    <LinkNext href={href} className="flex flex-col gap-4">
+    <LinkNext href={`/courses/${slug}`} className="flex flex-col gap-4 group">
       <motion.div
         className="relative"
         onHoverStart={() => setHover(true)}
         onHoverEnd={() => setHover(false)}
       >
         <img
-          src={image}
-          className="w-full object-cover h-[200px] rounded-2xl"
+          src={`${process.env.NEXT_PUBLIC_STRAPI_URL}${cover.data.attributes.url}`}
+          className="w-full object-cover h-[200px] rounded-2xl group-hover:brightness-50 transition-all"
           alt=""
         />
         <AnimatePresence>
@@ -58,26 +59,3 @@ const CourseCard = ({ image, name, href }) => {
     </LinkNext>
   )
 }
-
-const courseList = [
-  {
-    name: 'Curso de Xadrez',
-    image: 'https://via.placeholder.com/150',
-    href: '/#',
-  },
-  {
-    name: 'Curso de Xadrez2',
-    image: 'https://via.placeholder.com/150',
-    href: '/#',
-  },
-  {
-    name: 'Curso de Xadrez3',
-    image: 'https://via.placeholder.com/150',
-    href: '/#',
-  },
-  {
-    name: 'Curso de Xadre4',
-    image: 'https://via.placeholder.com/150',
-    href: '/#',
-  },
-]
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 import { motion } from 'framer-motion'
 import { action, line, menuDirection } from './anim'
-
+import useSWR from 'swr'
 import { useState } from 'react'
 import { anim } from '@/styles/helpers'
 import { Background } from './Background'
@@ -9,11 +9,31 @@ import { Logo } from './Logo'
 import { ButtonAction } from './ButtonAction'
 import { Navigation } from './Navigation'
 import { useScrollDirection } from '@/hooks'
+import { strapi } from '@/api'
+
+const getCourses = async (url) => {
+  const response = await strapi.get(url, {
+    searchParams: {
+      populate: '*',
+      'pagination[pageSize]': 4,
+    },
+  })
+  const _res = await response.json()
+
+  return _res.data.map((e) => e.attributes)
+}
 
 export const Header = () => {
   const [menuActive, setMenuActive] = useState(null)
   const dir = useScrollDirection()
   const showStatus = dir === 'up' ? 'show' : 'hide'
+
+  const { data: courses, isLoading: loadingCourses } = useSWR(
+    'courses',
+    getCourses,
+  )
+
+  console.log(courses)
 
   return (
     <>
@@ -29,14 +49,14 @@ export const Header = () => {
         <div className="container mx-auto h-header-height flex items-center justify-between">
           <div className="flex gap-24 items-center max-sm:flex-1 max-sm:justify-between">
             <Logo />
-            <Navigation handleMenu={setMenuActive} />
+            <Navigation courses={courses} handleMenu={setMenuActive} />
           </div>
           <motion.div {...anim(action)} className="max-sm:hidden">
             <ButtonAction>Acessar Conteúdo</ButtonAction>
           </motion.div>
         </div>
         <div className="h-[1px] bg-gray-300 w-full" />
-        <Menu show={Boolean(menuActive)} />
+        <Menu show={Boolean(menuActive)} data={courses} />
       </motion.div>
       <Background show={Boolean(menuActive)} />
     </>
